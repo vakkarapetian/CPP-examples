93347c28bf72503b910140f93873d40438b4f3ef
Vak Karapetian <57267892+vakkarapetian@users.noreply.github.com>
2020-08-23 16:16:30 +0400

    Create Calculate Average and Percentage Marks.cpp

---
+++ Calculate Average and Percentage Marks.cpp
@@ -0,0 +1,22 @@
+/* C++ Program - Calculate Average and Percentage Marks */
+		
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int mark[5], i;
+	float sum=0;
+	cout<<"Enter marks obtained in Physics, Chemistry, Maths, CS, English :";
+	for(i=0; i<5; i++)
+	{
+		cin>>mark[i];
+		sum=sum+mark[i];
+	}
+	float avg=sum/5;
+	float perc;
+	perc=(sum/500)*100;
+	cout<<"Average Marks = "<<avg;
+	cout<<"\nPercentage = "<<perc<<"%";
+	getch();
+}
