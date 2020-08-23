a307f91e2b4333614c76244762f679fa04af0e0d
Vak Karapetian <57267892+vakkarapetian@users.noreply.github.com>
2020-08-23 16:14:56 +0400

    Create Addition, Subtraction, Multiplication, Division.cpp

---
+++ Addition, Subtraction, Multiplication, Division.cpp
@@ -0,0 +1,20 @@
+/* C++ Program - Addition, Subtraction, Multiplication, Division */
+		 
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a, b, res;
+	cout<<"Enter two number :";
+	cin>>a>>b;
+	res=a+b;
+	cout<<"\nAddition = "<<res;
+	res=a-b;
+	cout<<"\nSubtraction = "<<res;
+	res=a*b;
+	cout<<"\nMultiplication = "<<res;
+	res=a/b;
+	cout<<"\nDivision = "<<res;
+	getch();
+}
